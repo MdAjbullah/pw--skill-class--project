bb54a71ffaf5fa19c24779f53766b857725f32c9
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2024-01-28 15:01:28 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,41 @@
+function grating(){
+    console.log("hi frined ");
+    console.log("how are you");
+}
+grating();
+// input in function
+
+
+function sqr(num){
+    let  result = (num * num);
+    console.log(result);
+}
+sqr(5);//-> 8 is agrument 
+
+    let x= sqr(8)
+    // let  result = (8 * 8);
+    console.log(x);//with the undefined also print 
+
+
+
+
+
+//function with without parameter and agrgument and retum value
+
+
+function saymessage(){
+    console.log("l love you ");
+}
+
+
+saymessage();
+saymessage();
+saymessage();
+saymessage();
+
+
+
+
+
+
+
