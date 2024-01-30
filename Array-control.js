a81236ac5441a8b090672f1f04855637ff041eb2
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2024-01-30 22:27:55 +0530

    array calculatore

---
+++ Array-control.js
@@ -0,0 +1,13 @@
+// let arr=[1,2,3,4,5,6,7,8,9,10];
+// arr.forEach((element,index,arr)=>{
+//     console.log(element,index,arr);
+// });
+
+// arr.forEach((element,index,arr)=>{
+//     arr[index]=element**2;
+//});
+
+
+var x;
+console.log(x); 
+x = 25;
